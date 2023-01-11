4d27aace60a3fc48de095a5fa344c4937fa774c2
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2023-01-11 15:17:49 +0200

    added task type column

--- src/composables/updatetransactions.js
+++ src/composables/updatetransactions.js
@@ -25,12 +25,16 @@ export async function useGetAllTransactions() {
     const project_id = ref('')
     const transaction_json_url = ref('')
     const md_version = ref('')
+    const total_ada = ref('')
+    const total_gmbl = ref('')
+    const total_agix = ref('')
   
     const contributions = ref([])
     const task_creator = ref('')
     const task_name = ref('')
     const task_label = ref('')
     const task_description = ref('')
+    const task_type = ref('')
     const exchange_rate = ref(0)
     const contributor_id = ref('')
     const contribution_id = ref('')
@@ -44,16 +48,22 @@ export async function useGetAllTransactions() {
     const updated_atx = ref([])
     const transaction_idx = ref([])
     const { oldWallets, oldWalletIds } = await useOldWallets()
+    const result = ref("")
 
-    async function changeToOldWallet(name) { //use this function to assign old names to addresses
+    async function changeToOldWallet(name) {
+      result.value = "" //use this function to assign old names to addresses
       console.log("testing function",name)
-      let result = ref("no__id")
+      
       for (let i in oldWallets.value) {
         if (name == oldWalletIds.value[i]) {
           result.value = oldWallets.value[i].substring(oldWallets.value[i].length - 6)
-        } else {
-          result.value = "no__id"
+          console.log("went through if",result.value)
+          break;
         }
+      }
+      if (result.value === "") {
+        result.value = "no__id"
+        console.log("went through else",result.value)
       }
       
       return result.value
@@ -164,7 +174,38 @@ export async function useGetAllTransactions() {
           loading.value = false
         }
       }
-    
+
+      function getTaskType(name, label, description) {
+        var tasktypes = {
+          "Operations":["Operations","PM Meeting","Video Meeting","Marketing Call","Weekly call - Treasury Guild Team","Weekly Call - Swarm & Treasury Guild","Setting up","set up","Schedule","setup","Organiz","gdrive","miro","Community Suggestion","Management","Transactions","Install"],
+          "Swarm Session":["Swarm Session","Join Saturday Swarm Session"],
+          "Insight fest":["Insight fest"],
+          "Content Creation":["Content Creation","article","Poetry","create","creating","Promotion","videocreation","Translat","Clip & Edit"],
+          "ATH Participation":["ATH Participation","join us at After Town Hall"],
+          "Onboarding":["Onboarding","Onboard"],
+          "Timestamping":["Timestamping","timestamp"],
+          "Documentation":["Documentation","How to","Report","mapping","Walkthrough"],
+          "Community Communication":["Community Communication","weekly call","Hosts","coordinat","Ambassador Town Hall","weekly meeting","community council","meeting","session","Facilitate","Announce"],
+          "Governance":["Governance","voting"],
+          "Tool Development":["Tool Development","MVP","Discord Server","Integrate","Add csv features","metadata"],
+          "Ideation":["Ideation","Suggest"],
+          "Fixed costs":["Fixed costs","Comm Org Tools","Zoom","GitBook"]
+        }
+        var finalResult = "";
+        for (let i in tasktypes) {
+          if (tasktypes[i].some(s => name.includes(s))) {
+            finalResult = i
+          }
+          if (tasktypes[i].some(s => label.includes(s))) {
+            finalResult = i
+          }
+          if (tasktypes[i].some(s => description.includes(s))) {
+            finalResult = i
+          }
+        }
+        return finalResult;
+      }
+
       async function updateTransactions() {
          
         const { transactions, transactionsUrls, transactionDates } =  await useGetTxs(orgEl, repoEl, projectJ.value, fundJ.value, poolJ.value)
@@ -176,13 +217,25 @@ export async function useGetAllTransactions() {
             transaction_date.value = transactionDates.value[i]
             
               if (transactions.value[i].exchangeRate) {
-                md_version.value = '0.01'
+                md_version.value = '0.02'
                 exchange_rate.value = parseFloat(transactions.value[i].exchangeRate)
+                total_ada.value = transactions.value[i].ada
+                total_gmbl.value = transactions.value[i].gmbl
+                total_agix.value = transactions.value[i].agix
               } else if (transactions.value[i].mdVersion) {
                 md_version.value = transactions.value[i].mdVersion[0] ? transactions.value[i].mdVersion[0] : "1.0"
                 console.log(parseFloat(((transactions.value[i].msg[transactions.value[i].msg.length - 2]).match(/[+-]?\d+(\.\d+)?/g))[0]))
                 exchange_rate.value = (parseFloat(((transactions.value[i].msg[transactions.value[i].msg.length - 2]).match(/[+-]?\d+(\.\d+)?/g))[0]))
-              } else { exchange_rate.value = 0.00 }
+                total_ada.value = transactions.value[i].msg.some(str => str.includes("USD") && str.includes("ADA"))?((transactions.value[i].msg[transactions.value[i].msg.findIndex(str => str.includes("USD") && str.includes("ADA"))]).match(/[+-]?\d+(\.\d+)?/g).map(parseFloat))[1]:(((transactions.value[i].msg[transactions.value[i].msg.findIndex(str => str.includes("Recipients"))]).match(/[+-]?\d+(\.\d+)?/g).map(parseFloat))[0]*1.344798).toFixed(2)
+                total_gmbl.value = transactions.value[i].msg.some(str => str.includes("USD") && str.includes("GMBL" || "gimbal"))?(((transactions.value[i].msg[transactions.value[i].msg.findIndex(str => str.includes("USD") && str.includes("GMBL" || "gimbal"))]).match(/[+-]?\d+(\.\d+)?/g).map(parseFloat))[1]):0
+                total_agix.value = transactions.value[i].msg.some(str => str.includes("USD") && str.includes("AGIX"))?(((transactions.value[i].msg[transactions.value[i].msg.findIndex(str => str.includes("USD") && str.includes("AGIX"))]).match(/[+-]?\d+(\.\d+)?/g).map(parseFloat))[1]):0
+              } else { 
+                exchange_rate.value = 0.00
+                md_version.value = '0.01'
+                total_ada.value = transactions.value[i].ada
+                total_gmbl.value = transactions.value[i].gmbl
+                total_agix.value = transactions.value[i].agix
+              }
     
             try {
           loading.value = true
@@ -195,6 +248,9 @@ export async function useGetAllTransactions() {
             transaction_date: transaction_date.value,
             exchange_rate: exchange_rate.value,
             updated_at: new Date(),
+            total_ada: total_ada.value,
+            total_gmbl: total_gmbl.value,
+            total_agix: total_agix.value
           }
     
           let { data, error } = await supabase
@@ -225,7 +281,7 @@ export async function useGetAllTransactions() {
                 task_name.value = transactions.value[i].contributions[k].name?transactions.value[i].contributions[k].name.join(' '):''
                 task_label.value = transactions.value[i].contributions[k].label?transactions.value[i].contributions[k].label:''
                 task_description.value = transactions.value[i].contributions[k].description?transactions.value[i].contributions[k].description.join(' '):''
-    
+                task_type.value = getTaskType(task_name.value,task_label.value,task_description.value)
                 try {
                   loading.value = true
             
@@ -235,6 +291,7 @@ export async function useGetAllTransactions() {
                     task_name: task_name.value,
                     task_label: task_label.value,
                     task_description: task_description.value,
+                    task_type: task_type.value
                   }
             
                   let { data, error } = await supabase
@@ -300,6 +357,7 @@ export async function useGetAllTransactions() {
               task_name.value = ''
               task_label.value = transactions.value[i].budget
               task_description.value = transactions.value[i].description
+              task_type.value = getTaskType(task_name.value,task_label.value,task_description.value)
               exchange_rate.value = parseFloat(transactions.value[i].exchangeRate)
               contributor_id.value = transactions.value[i].name
               ada.value = transactions.value[i].ada ? transactions.value[i].ada : 0
@@ -325,6 +383,7 @@ export async function useGetAllTransactions() {
                     task_name: task_name.value,
                     task_label: task_label.value,
                     task_description: task_description.value,
+                    task_type: task_type.value
                   }
             
                   let { data, error } = await supabase
